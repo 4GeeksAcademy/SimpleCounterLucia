90c12f8f444cd74b40bddcab8be0d4144cf518b9
Luica <138072443+lumartins311@users.noreply.github.com>
2023-08-16 11:43:07 +0000

    reloj

--- src/js/component/SecondsCounter.jsx
+++ src/js/component/SecondsCounter.jsx
@@ -3,15 +3,15 @@ import React from "react";
 
 const SecondsCounter = ({ digito, digito2, digito3, digito4, digito5, digito6 }) => {
     return (
-        <div className="container w-75 p-3 justify-content-center bg-dark">
-            <div className="row row-cols-6 w-100 p-3 align-content-center mx-1 px-0 py-0">
-                <div className="col mx-0 px-0"><h1 className="text-center mt-5" style={{ fontSize: "100px", marginRight: "50px", color: "white" }}>{digito6}</h1></div>
-                <div className="col mx-0 px-0"><h1 className="text-center mt-5" style={{ fontSize: "100px", marginRight: "50px", color: "white" }}>{digito5}</h1></div>
-                <div className="col mx-0 px-0"><h1 className="text-center mt-5" style={{ fontSize: "100px", marginRight: "50px", color: "white" }}>{digito4}</h1></div>
-                <div className="col mx-0 px-0"><h1 className="text-center mt-5" style={{ fontSize: "100px", marginRight: "50px", color: "white" }}>{digito3}</h1></div>
-                <div className="col mx-0 px-0"><h1 className="text-center mt-5" style={{ fontSize: "100px", marginRight: "50px", color: "white" }}>{digito2}</h1></div>
-                <div className="col mx-0 px-0"><h1 className="text-center mt-5" style={{ fontSize: "100px", color: "white" }}>{digito}</h1></div>
-
+        <div className="container w-75 p-3 justify-content-center">
+            <div className="row row-cols-7 w-100 p-3 align-content-center mx-1 px-0 py-0">
+                <div className="col mx-0 px-0 bg-dark"><i className="fa fa-clock text-center my-5 text-white" style={{ fontSize: "100px" }}></i></div>
+                <div className="col mx-0 px-0 bg-dark"><h1 className="text-center mt-5" style={{ fontSize: "100px", marginRight: "50px", color: "white" }}>{digito6}</h1></div>
+                <div className="col mx-0 px-0 bg-dark"><h1 className="text-center mt-5" style={{ fontSize: "100px", marginRight: "50px", color: "white" }}>{digito5}</h1></div>
+                <div className="col mx-0 px-0 bg-dark"><h1 className="text-center mt-5" style={{ fontSize: "100px", marginRight: "50px", color: "white" }}>{digito4}</h1></div>
+                <div className="col mx-0 px-0 bg-dark"><h1 className="text-center mt-5" style={{ fontSize: "100px", marginRight: "50px", color: "white" }}>{digito3}</h1></div>
+                <div className="col mx-0 px-0 bg-dark"><h1 className="text-center mt-5" style={{ fontSize: "100px", marginRight: "50px", color: "white" }}>{digito2}</h1></div>
+                <div className="col mx-0 px-0 bg-dark"><h1 className="text-center mt-5" style={{ fontSize: "100px", color: "white" }}>{digito}</h1></div>
             </div>
         </div>
     )
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -9,8 +9,8 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = ({ digito, digito2, digito3, digito4, digito5, digito6 }) => {
 	return (
-		<div className="text-center bg-dark" style={{ height: "250px" }}>
-			<div style={{ height: "30px", width:"30px" }}><app /></div>
+		<div className="text-center" style={{ height: "300px" }}>
+			<div style={{ height: "30px", width: "30px" }}></div>
 			<SecondCounter digito={digito} digito2={digito2} digito3={digito3} digito4={digito4} digito5={digito5} digito6={digito6} />
 			{/* <secondCounter digito/> */}
 			{/* <h1 className="text-center mt-5">{digito}</h1>
